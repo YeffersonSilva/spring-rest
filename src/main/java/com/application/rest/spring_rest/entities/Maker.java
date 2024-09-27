992e7be6e87dff78632fd7db3be58613c060fb8b
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-27 19:45:10 -0300

    fix: fix name bug in entities

--- src/main/java/com/application/rest/spring_rest/entities/Maker.java
+++ src/main/java/com/application/rest/spring_rest/entities/Maker.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -29,6 +30,8 @@ public class Maker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre")
     private String name;
 
     @OneToMany(mappedBy = "maker", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
