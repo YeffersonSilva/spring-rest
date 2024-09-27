934278187108535e82565e6b65d29f6f3a3bd5a7
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-27 19:30:42 -0300

    fix: fix issue with database connection

--- src/main/java/com/application/rest/spring_rest/entities/Product.java
+++ src/main/java/com/application/rest/spring_rest/entities/Product.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,7 +35,8 @@ public class Product {
     @Column(name = "precio")
     private BigDecimal price;
 
-
-    
+    @ManyToOne
+   @JoinColumn(name = "id_fabricante", nullable = false) 
+    private Maker maker;
 
 }
